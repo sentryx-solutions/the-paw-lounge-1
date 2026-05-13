e0583297c5e551df471c91d1319527d224c29d8c
naseerai <naseeralisk786@gmail.com>
2026-05-13 14:34:13 +0530

    whatsapp changed from to right

--- pages/index.js
+++ pages/index.js
@@ -48,7 +48,7 @@ export default function Home() {
   href="https://wa.me/919346269204?text=Hello%20Pawvera!"
   target="_blank"
   rel="noopener noreferrer"
-  className="fixed bottom-6 left-6 z-50 bg-green-500 hover:bg-green-600 text-white p-4 rounded-full shadow-lg transition duration-300"
+  className="fixed bottom-6 right-6 z-50 bg-green-500 hover:bg-green-600 text-white p-4 rounded-full shadow-lg transition duration-300"
 >
   <FaWhatsapp size={30} />
 </a>
